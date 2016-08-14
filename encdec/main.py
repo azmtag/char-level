2cec6e0f63d021d2b7d165248b04af4267b93c14
alexeyev <anton.m.alexeyev@gmail.com>
2016-08-15 01:12:35 +0300

    Reducing recovery size

--- encdec/main.py
+++ encdec/main.py
@@ -62,7 +62,7 @@ encoder_model_name_path = 'params/et2v_model.json'
 encoder_model_weights_path = 'params/et2v_model_weights.h5'
 
 # Maximum length. Longer gets chopped. Shorter gets padded.
-maxlen = 6
+maxlen = 8
 
 # Filters for conv layers
 nb_filter = 512
