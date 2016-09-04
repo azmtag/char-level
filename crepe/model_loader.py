9cb253bbec68e403710a1a570a157503b677229b
snikolenko <snikolenko@gmail.com>
2016-09-04 14:23:34 +0300

    model_loader takes all epochs from a model

--- crepe/model_loader.py
+++ crepe/model_loader.py
@@ -1,13 +1,13 @@
 # coding=utf-8
 """
 Model loader and tested. Usage example
-$ python3 model_loader.py --pref pycrepe.64.00 --models-path /home/snikolenko/soft/char-level/crepe/models_senti/
+$ KERAS_BACKEND=tensorflow python3 model_loader.py --pref pycrepe.64 --models-path /home/snikolenko/soft/char-level/crepe/models_senti/
 
 """
 import argparse as ap
 import datetime
 import json
-import os
+import os,glob
 
 import keras.backend.tensorflow_backend as KTF
 import tensorflow as tf
@@ -40,17 +40,22 @@ parser.add_argument('--batch', type=int,
                     help='default=80; test batch size')
 
 parser.add_argument('--gpu_fraction', type=float,
-                    default=0.3,
-                    help='default=0.3; GPU fraction, please, use with care')
+                    default=0.15,
+                    help='default=0.15; GPU fraction, please, use with care')
 
 args = parser.parse_args()
 
+def my_print(s):
+    print("[" + str(datetime.datetime.now()) + "] " + s)
+
+
 # ====== GPU SESSION =====================
 
-print('GPU fraction ' + str(args.gpu_fraction))
-print('models path', args.models_path)
-print('dataset', args.dataset)
-print('config', args.pref)
+my_print('GPU fraction ' + str(args.gpu_fraction))
+my_print('models path %s' % args.models_path)
+my_print('dataset %s' % args.dataset)
+my_print('config %s' % args.pref)
+
 
 
 def get_session(gpu_fraction=args.gpu_fraction):
@@ -67,33 +72,34 @@ def get_session(gpu_fraction=args.gpu_fraction):
         return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
 
 
+def get_metrics(model, test_batches):
+    start = datetime.datetime.now()
+    test_acc = 0.0
+    test_loss = 0.0
+    test_step = 1
+    test_loss_avg = 0.0
+    test_acc_avg = 0.0
+
+    for x_test_batch, y_test_batch in test_batches:
+        f_ev = model.test_on_batch(x_test_batch, y_test_batch)
+        test_loss += f_ev[0]
+        test_loss_avg = test_loss / test_step
+        test_acc += f_ev[1]
+
+        test_acc_avg = test_acc / test_step
+        test_step += 1
+
+    stop = datetime.datetime.now()
+    e_elap = stop - start
+    return test_loss_avg, test_acc_avg, e_elap
+    # my_print('Loss: {}\nAccuracy: {}\nTotal time: {}\n'.format(test_loss_avg, test_acc_avg, e_elap))
+
+
 KTF.set_session(get_session())
 
 # ============= VOCAB =============
 
 vocab, reverse_vocab, vocab_size, check = data_helpers.create_vocab_set()
-
-# ============= MODEL ===============
-
-print("Loading model from", args.models_path + "/" + args.pref + ".*")
-
-json_file = open(args.models_path + "/" + args.pref + ".json", 'r')
-model_as_json = json.load(json_file, encoding="UTF-8")
-json_file.close()
-
-model = model_from_json(model_as_json)
-model.load_weights(args.models_path + "/" + args.pref + ".h5")
-
-if args.optimizer == 'adam':
-    optimizer = Adam()
-else:
-    optimizer = args.optimizer
-
-print("Chosen optimizer: ", optimizer, "compiling now...")
-
-model.compile(optimizer=optimizer, loss=args.loss, metrics=['accuracy'])
-
-print("Model loaded and compiled.")
 
 # ============= TEST DATA =============
 
@@ -105,27 +111,33 @@ else:
 # ============= EVAL ==================
 
 xi_test, yi_test = data_helpers.shuffle_matrix(x_test, y_test)
-test_batches = data_helpers.mini_batch_generator(xi_test, yi_test, vocab,
-                                                 vocab_size, check, model.input_shape[1],
-                                                 batch_size=int(args.batch))
+test_batches = None 
+my_print("Dataset %s loaded" % args.dataset)
+# ============= MODEL ===============
 
-start = datetime.datetime.now()
-test_acc = 0.0
-test_loss = 0.0
-test_step = 1
-test_loss_avg = 0.0
-test_acc_avg = 0.0
+my_print("Loading models from %s..." % (args.models_path + "/" + args.pref + ".*") )
 
-for x_test_batch, y_test_batch in test_batches:
-    f_ev = model.test_on_batch(x_test_batch, y_test_batch)
-    test_loss += f_ev[0]
-    test_loss_avg = test_loss / test_step
-    test_acc += f_ev[1]
+fnames_json = sorted(glob.glob(args.models_path + '/' + args.pref + '*json'))
+for fname_json in fnames_json:
+    json_file = open(fname_json, 'r')
+    model_as_json = json.load(json_file, encoding="UTF-8")
+    json_file.close()
 
-    test_acc_avg = test_acc / test_step
-    test_step += 1
+    model = model_from_json(model_as_json)
+    model.load_weights(fname_json[:-4] + "h5")
 
-stop = datetime.datetime.now()
-e_elap = stop - start
+    if args.optimizer == 'adam':
+        optimizer = Adam()
+    else:
+        optimizer = args.optimizer
 
-print('Loss: {}\nAccuracy: {}\nTotal time: {}\n'.format(test_loss_avg, test_acc_avg, e_elap))
+    # my_print("Chosen optimizer: ", optimizer, "compiling now...")
+    model.compile(optimizer=optimizer, loss=args.loss, metrics=['accuracy'])
+
+    test_batches = data_helpers.mini_batch_generator(xi_test, yi_test, vocab,
+                         vocab_size, check, model.input_shape[1],
+                         batch_size=int(args.batch))
+
+    # my_print("Model loaded and compiled.")
+    test_loss_avg, test_acc_avg, e_elap = get_metrics(model, test_batches)
+    print("%s\t%.4f\t%.4f" % (fname_json.split('/')[-1], test_loss_avg, test_acc_avg))
